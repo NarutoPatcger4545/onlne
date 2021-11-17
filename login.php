f4b0b71e643ce688b06302494c6c0aa52847f2f1
NarutoPatcger4545 <90080904+NarutoPatcger4545@users.noreply.github.com>
2021-11-17 11:10:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,11 @@
-<?PHP
-$data = '';
+<?php
 
-$user = 'user';
-$pass = 'pass';
+$ackdata = array("is_conect" => true,
 
-if (!empty($_REQUEST) ) {
-    if (!empty($_REQUEST[$user]) && !empty($_REQUEST[$pass])) {
-        if ($_REQUEST[$user] == 'testuser' && $_REQUEST[$pass] == 'testpass') {
-            $data = '1|'.md5($_REQUEST[$user] . $_REQUEST[$pass]).'|User: ' . $_REQUEST[$user] . ' successfully logged in!';
-        }else{
-            $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Incorrect username or password!';
-        }
-    }else{
-        $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Fill in username and password!';
-    }
-}
+"user" => "ZeClayX",
 
-header('Content-Type: text/plain');
-echo $data;
+"pass" => "2021");
+
+echo json_encode($ackdata);
+
+?>
